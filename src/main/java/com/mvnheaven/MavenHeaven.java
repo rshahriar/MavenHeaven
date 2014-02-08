de538ff75ad170b1453d593d03647fabd0644bb5
Rakib Shahriar <rakib.shahriar@cefalo.com>
2014-02-08 16:07:12 +0600

    Initial commit

---
+++ src/main/java/com/mvnheaven/MavenHeaven.java
@@ -0,0 +1,12 @@
+package com.mvnheaven;
+
+/**
+ * Created with IntelliJ IDEA. User: rakib Date: 2/8/14 Time: 1:27 PM To change this template use
+ * File | Settings | File Templates.
+ */
+public class MavenHeaven {
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Maven Heaven!" );
+    }
+}
